97233b34ec5a07a8fad9bc3ec54e57088bb10a5d
syuliastuti214-dotcom <syuliastuti214@gmail.com>
2026-05-16 23:46:42 +0700

    Align branding and palette with BUF logo

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./styles.css";
+import "./brand-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
